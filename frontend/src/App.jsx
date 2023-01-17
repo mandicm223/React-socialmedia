8087c014698a46731896f37afc55157235fb9882
Marko Mandic <mandicm223@gmail.com>
2023-01-17 09:39:08 +0100

    setuped frotend also

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+const App = () => {
+  return (
+    <div className="underline text-5xl">
+      <h1>Vite + React</h1>
+    </div>
+  )
+}
+
+export default App
