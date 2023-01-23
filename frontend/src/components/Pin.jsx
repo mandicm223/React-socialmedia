66edd4e7031598f9f309473f1acc31e5b509e820
Marko Mandic <mandicm223@gmail.com>
2023-01-23 15:19:45 +0100

    started working on createPin page

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -117,6 +117,15 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save }}) => {
 
       </div>
 
+      <Link to={`user-profile/${postedBy?._id}`} className="flex gap-2 mt-2 items-center">
+                  <img 
+                    className='w-8 h-8 rounded-full object-cover'
+                    src={postedBy?.image}
+                    alt="user=profile"
+                  />
+                  <p className='font-semibold capitalize'>{postedBy?.userName}</p>
+      </Link>
+
 
 
     </div>
