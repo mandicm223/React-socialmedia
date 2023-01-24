ead9b0b57273fd939c540cb628f7d86b367af12f
Marko Mandic <mandicm223@gmail.com>
2023-01-24 18:54:20 +0100

    added forms to cratePin and implemented uploadPin functionality

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -42,19 +42,54 @@ const CreatePin = ({ user }) => {
     }
   }
 
+  const uploadPin = () => {
+    if(title && about && destination && imageAsset?._id && category) {
+      const doc = {
+        _type: 'pin',
+        title,
+        about,
+        destination,
+        image: {
+          _type: 'image',
+          asset: {
+            _type: 'reference',
+            _ref: imageAsset?._id
+          }
+        },
+        userId: user._id,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user._id
+        },
+        category
+      }
+
+      client.create(document)
+        .then(() => {
+          navigate('/')
+        })
+    } else {
+      setFields(true);
+
+      setTimeout(() => {
+        setFields(false)
+      }, 2000);
+    }
+  }
+
   return (
     <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
       {fields && (
         <p className='text-red-500 b-5 text-xl transition-all duration-150 ease-in'>Please fill in all the fields</p>
       )}
       <div className='flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full'>
-        <div className='bg-secondaryColor p-3 flex flex-0.7 w-full'>
+        <div className='bg-secondaryColor p-3 flex flex-0.7 w-full cursor-pointer'>
           <div className='flex flex-col justify-center items-center border-2 border-dotted border-gray-300 p-3 w-full h-420'>
             {loading && <Spinner />}
             {wrongImageType && <p>Wrong image type</p>}
             {!imageAsset ? (
               <label>
-                <div className='flex flex-col items-center justify-center h-full'>
+                <div className='flex flex-col items-center justify-center h-full cursor-pointer'>
                   <div className='flex flex-col justify-center items-center'>
                     <p className='font-bold text-2xl'>
                       <AiOutlineCloudUpload />
@@ -80,6 +115,60 @@ const CreatePin = ({ user }) => {
               </div>
             )}
           </div>
+        </div>
+
+        <div className='flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full'>
+              <input 
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Add your title"
+                className='outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200 p-2'
+              />
+              {user && (
+                <div className='flex gap-2 my-2 items-center bg-white rounded-lg'>
+                  <img src={user.image} alt="user-profile" className='w-10 h-10 rounded-full'/>
+                  <p className='font-bold'>{user.userName}</p>
+                </div>
+              )}
+              <input 
+                type="text"
+                value={about}
+                onChange={(e) => setAbout(e.target.value)}
+                placeholder="What is your pin about"
+                className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2'
+              />
+               <input 
+                type="text"
+                value={destination}
+                onChange={(e) => setDestination(e.target.value)}
+                placeholder="Add a destination link"
+                className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2'
+              />
+              <div className='flex flex-col'>
+                <div>
+                  <p className='mb-2 font-semibold text-lg sm:text-xl'>Choose Pin Category</p>
+                  <select 
+                    onChange={(e) => setCategory(e.target.value)}
+                    className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+                  >
+                    <option value="other" className='bg-white'>Select category</option>
+                    
+                    {categories.map((categorie) => (
+                      <option value={categorie.name} className='text-base border-0 outline-none capitalize bg-white text-black'>{categorie.name}</option>
+                    ))}
+                  </select>
+                </div>
+                <div className='flex justify-end items-end mt-5'>
+                      <button
+                        type='button'
+                        onClick={uploadPin}
+                        className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none"
+                      >
+                        Save Pin
+                      </button>
+                </div>
+              </div>
         </div>
       </div>
     </div>
