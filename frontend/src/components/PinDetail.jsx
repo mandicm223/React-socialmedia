077c81ff1a38882c4ad98fe9387e042c2e1fd719
Marko Mandic <mandicm223@gmail.com>
2023-01-24 19:50:19 +0100

    started working on pinDetail page

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -1,6 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { MdDownloadForOffline } from 'react-icons/md'
+import { Link, useParams } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
 
-const PinDetail = () => {
+import { client, urlFor } from '../client'
+import MasonryLayout from './MasonryLayout'
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data'
+import Spinner from './Spinner'
+
+const PinDetail = ({ user }) => {
+
+  const [pins, setPins] = useState(null);
+  const [pinDetail, setPinDetail] = useState(null);
+  const [comment, setComment] = useState('');
+  const [addingComment, setAddingComment] = useState(false);
+
+  const { pinId } = useParams();
+
+  const fetchPinDetais = () => {
+    let query = pinDetailQuery(pinId)
+
+    if(query){
+      client.fetch(query)
+        .then((data => {
+          setPinDetail(data[0]);
+
+          if(data[0]) {
+            query = pinDetailMorePinQuery(data[0])
+
+            client.fetch(query)
+              .then((res) => setPins(res))
+          }
+        }))
+    }
+  }
+
+  useEffect(() => {
+    fetchPinDetais()
+  }, [pinId]);
+
+
+  if(!pinDetail) return <Spinner message="Loading pin..." />
+
   return (
     <div>
       PinDetail
